5150d5525e97825a469c4b6d5433e512fb89c80b
Matii29 <145938916+Matii29@users.noreply.github.com>
2023-11-12 23:47:47 +0100

    Delete zad5.py

--- zad5.py
+++
@@ -1,9 +0,0 @@
-# Zsumuj wszystkie liczby które są są podzielne przez 5 do wartości podanej przez użytkownika.
-suma = 0
-inp = int(input("Podaj liczbę: "))
-i = 0
-while i <= inp:
-    if i%5 == 0:
-        suma += i
-    i += 132
-print(suma) 
