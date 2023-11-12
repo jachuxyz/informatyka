30619636f352531a8601b4c5cf27c0bd99c51c46
Matii29 <145938916+Matii29@users.noreply.github.com>
2023-11-12 23:47:39 +0100

    Delete zad2.py

--- zad2.py
+++
@@ -1,7 +0,0 @@
-# Napiszmy program który wyświetli nam wszystkie liczby podzielne przez 9. Do wartości podanej przez użytkownika.
-inp = int(input("Podaj liczbę: "))
-i = 0
-while i <= inp:
-    if i%9 == 0:
-        print(i)
-    i += 1
